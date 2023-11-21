547d20874e1032905040f1554ff0d66a22a418fc
psterner <psterner@hotmail.com>
2023-11-21 19:48:38 +0100

    Add storeSlices cart and product

---
+++ src/reducers/cart.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cart = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push({ ...action.payload, quantity: 1 });
+    },
+  },
+});
